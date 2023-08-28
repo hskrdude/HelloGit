6608aafd944e43c7d4f5abb46571df328dbfdb26
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2023-08-27 19:47:27 -0500

    added my name and my major

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+//Author: Evan Howell
+//Date: 8/27/2023
+//Purpose: complete java hack 01
+
+public class Hello {
+	public static void main (String args[]) {
+			System.out.println("Evan Howell");
+			System.out.println("Computer Science");
+	}
+}
