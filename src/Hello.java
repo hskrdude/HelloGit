0171d531b8d9a8e7551a0cf5e1154e1843be82bf
Evan <Evan@DESKTOP-G413O5R.unl.edu>
2023-08-27 19:29:52 -0500

    added major to printout

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+//Author: Evan Howell
+//Date: 8/27/2023
+//Purpose: complete java hack 01
+
+public class Poggers {
+	public static void main (String args[]) {
+			System.out.println("Evan Howell");
+	}
+}
