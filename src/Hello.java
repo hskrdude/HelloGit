bcbd53ae22d3718b60d80a00166f639ac7e122f4
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2023-08-27 21:17:33 -0500

    added my name and major

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,12 @@
 //Purpose: complete java hack 01
 
 public class Hello {
-	public static void main (String args[]) {
-			System.out.println("Evan Howell");
-			System.out.println("Computer Science");
+	public static void main(String args[]) {
+		System.out.println("Evan Howell");
+		System.out.println("Computer Science");
+
+		System.out.println("Luciano Carvalho");
+		System.out.println("Computer Science");
+
 	}
 }
