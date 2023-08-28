7a9927548b2a8122fb93318b8afa45781795655c
Evan <Evan@DESKTOP-G413O5R.unl.edu>
2023-08-27 19:41:36 -0500

    Kowalski, analysis! Sir, poor class naming and a failure to push.

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,9 @@
 //Date: 8/27/2023
 //Purpose: complete java hack 01
 
-public class Poggers {
+public class Hello {
 	public static void main (String args[]) {
 			System.out.println("Evan Howell");
+			System.out.println("Computer Science");
 	}
 }
